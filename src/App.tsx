7cd14ad5e1cbebcad9d5dbc8cfe5a375c185402b
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-09-16 01:39:02 +0300

    Added logic for todo list. Moved type of todo to App component and throw it throught props and type it props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header/Header";
 import Panel from "./components/Panel/Panel";
 import {Box} from "@material-ui/core";
 import TodoList from "./components/TodoList/TodoList";
 
+export type Todo = {
+    id: number,
+    name: string,
+    description: string,
+    checked: boolean
+}
+
 function App() {
+    const [todoLists, setTodoLists] = useState([
+        {id: 1, name: 'task 1', description: 'Walk with a dog', checked: true},
+        {id: 2, name: 'task 2', description: 'Walk with a dogWalk with a dogWalk with a dog', checked: false},
+        {id: 3, name: 'task 3', description: 'Walk with a dogWalk with a dog', checked: true}
+
+    ])
+
   return (
       <div>
           <Box style={{width: '50%'}} display='flex' flexDirection='column'>
               <Header />
               <Panel />
-              <TodoList />
+              <TodoList todoLists={todoLists} />
           </Box>
       </div>
   );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,25 +1,17 @@
 import React, {FC} from "react";
 import {Box} from "@material-ui/core";
 import TodoItem from "./TodoItem/TodoItem";
-
-const todoLists = [
-    {id: 1, name: 'task 1', description: 'Walk with a dog', checked: true},
-    {id: 2, name: 'task 2', description: 'Walk with a dogWalk with a dogWalk with a dog', checked: false},
-    {id: 3, name: 'task 3', description: 'Walk with a dogWalk with a dog', checked: true}
-
-]
+import {Todo} from "../../App";
 
 type TodoListProps = {
-    id: number,
-    description: string,
-    checked: boolean
+    todoLists: Todo[]
 }
 
-const TodoList:FC = () => {
+const TodoList:FC<TodoListProps> = ({todoLists}) => {
     return(
         <Box>
             {
-                todoLists.map(todoList => <TodoItem todo={todoList} />)
+                todoLists.map(todoList => <TodoItem key={todoList.id} todo={todoList} />)
             }
         </Box>
     )
